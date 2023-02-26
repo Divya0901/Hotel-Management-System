03df1f71495459b9962edb331d553562c45d8c9f
Divya0901 <gujjukadivya0109@gmail.com>
2023-02-26 16:12:21 +0530

    First commit

---
+++ src/test/java/com/itc/hms/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
